2fa9e2a62eba337b565aa641c8cc58be1ca95414
musaini06 <45730154+musaini06@users.noreply.github.com>
2018-12-12 12:43:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBioeUuuCjbL-Tf869oJhsinaik4LMgarAhZtkhaOsy396e9ywuoKhtLWGkq2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "30a1058863a22d75";
+$os="android";
+$sign="75e653293a6fcc14abc1666bb69e0a1e391bb3e7922ae645cff13f242615c97b";
